e673b45a7c5b9d4f29cb0ba680cb63a87a5fc4b3
MrMariu5 <marius.sadreika@gmail.com>
2020-05-24 17:32:42 +0300

    Base

---
+++ CrawlerPiguLt/CrawlerPiguLt/Program.cs
@@ -0,0 +1,95 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerPiguLt
+{
+    public class Program
+    {
+        public int triesToConnect = 0;
+        public int crawling(String newUrlAddress)
+        {
+            WebClient client = new WebClient();
+
+            NameValueCollection myNameValueCollection = new NameValueCollection();
+
+            String[] valueArray = null;
+
+            myNameValueCollection.Add("Host", "pigu.lt");
+            myNameValueCollection.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0");
+            myNameValueCollection.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            myNameValueCollection.Add("Accept-Language", "en-GB,en;q=0.5");
+
+            foreach (String key in myNameValueCollection.Keys)
+            {
+                valueArray = myNameValueCollection.GetValues(key);
+                foreach (String value in valueArray)
+                {
+                    client.Headers.Set(key, value);
+                }
+            }
+
+            try
+            {
+                HtmlWeb hw = new HtmlWeb();
+                HtmlDocument data = hw.Load(newUrlAddress);
+                foreach (HtmlNode link in data.DocumentNode.SelectNodes("//div[@class='subcategory-list']//a"))
+                {
+                    Console.WriteLine(link.GetAttributeValue("href", string.Empty));
+                }
+                
+
+
+                //string data = client.DownloadString(newUrlAddress);
+                //Console.WriteLine(newUrlAddress);
+                //Console.WriteLine(data);
+
+                /*WebHeaderCollection myWebHeaderCollection = client.ResponseHeaders;
+                for (int i = 0; i < myWebHeaderCollection.Count; i++)
+                    Console.WriteLine("\t" + myWebHeaderCollection.GetKey(i) + " = " + myWebHeaderCollection.Get(i));
+
+                Console.WriteLine("Duomenys\n" + data);*/
+                return 0;
+
+            }
+            catch (Exception response_exception)
+            {
+                Console.WriteLine("ERROR\n" + response_exception);
+                triesToConnect = triesToConnect + 1;
+                if (triesToConnect == 1)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int methodRecursion = 1;
+            Program crawlerObject = new Program();
+            Sc scObject = new Sc();
+
+           // for (int i = 30; i < days; i++) // reikes pakeisti (int i = 1; i < days + 1; i++)
+           // {
+                string newUrlAddress = scObject.createUrl();
+
+                methodRecursion = crawlerObject.crawling(newUrlAddress);
+
+                if (methodRecursion == 1)
+                {
+                    methodRecursion = crawlerObject.crawling(newUrlAddress);
+                }
+          //  }
+            Console.WriteLine("END");
+        }
+    }
+}
---
+++ CrawlerPiguLt/CrawlerPiguLt/Sc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerPiguLt
+{
+    public class Sc
+    {
+        private string urlAddress = "https://pigu.lt/lt/katalogas";
+
+      //  private string departure_year_month = "2020-08";
+       // private string departure_day = "25";
+
+
+        public String createUrl()
+        {
+            /*string newUrlAddress = urlAddress + "apps/personas/booking?" + "fecha1_dia=" +
+                departure_day + "&fecha1_anomes=" + departure_year_month + "&fecha2_dia=" +
+                return_day + "&fecha2_anomes=" + return_year_month + "&from_city2=" +
+                city_code + "&to_city2=" + second_city_code +
+                "&auAvailability=1&ida_vuelta=ida&vuelos_origen=" +
+                city + "&from_city1=" + city_code + "&vuelos_destino=" +
+                second_city + "&to_city1=" + second_city_code + "&flex=1&vuelos_fecha_salida_ddmmaaaa=" + departure_day +
+                full_departure_date + "&vuelos_fecha_regreso_ddmmaaaa=" + return_day + full_return_date +
+                "&cabina=" + flightClass + "&nadults=" + adult_count + "&nchildren=" +
+                children_count + "&ninfants=0&cod_promo=&stopover_outbound_days=0&stopover_inbound_days=0&application=#/";*/
+
+            string newUrlAddress = urlAddress;
+
+            return newUrlAddress;
+        }
+    }
+}
